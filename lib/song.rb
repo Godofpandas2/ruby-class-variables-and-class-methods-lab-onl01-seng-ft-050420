746e589cc42e1c1668cfb18259eb8d29fcb37c8c
David Hutchison <dhutchis68516@gmail.com>
2020-05-14 17:21:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,22 +29,22 @@ class Song
 
   def self.genre_count
     genre_count = {}
-      @@genres.each do |genre, count|
+      @@genres.each do |genre|
         if genre_count[genre]
-          genre_count[count] +=1
+          genre_count[genre] +=1
         else
-          genre_count[count] +=1
+          genre_count[genre] +=1
         end
       end
   end
 
   def self.artist_count
     artist_count = {}
-    @@artists.each do |artist, count|
-      if artist_count[genre]
-        artist_count[count] += 1
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
       else
-        artist_count[count] += 1
+        artist_count[artist] += 1
       end
     end
   end
